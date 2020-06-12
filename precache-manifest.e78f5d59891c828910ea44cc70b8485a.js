5d86c455e7792d75eb411b2d7b0e7cdb4eba48f0
Louis Notteghem <louis.notteghem@isep.fr>
2020-06-12 13:22:35 +0200

    Updates

---
+++ precache-manifest.e78f5d59891c828910ea44cc70b8485a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48dbb5e53307a2e5f8d4df638a9d84ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d522e920704e125a2242",
+    "url": "/static/css/main.7d7630a1.chunk.css"
+  },
+  {
+    "revision": "630aedc7f467d944f2b5",
+    "url": "/static/js/2.023f8d41.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.023f8d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d522e920704e125a2242",
+    "url": "/static/js/main.b5875450.chunk.js"
+  },
+  {
+    "revision": "411d927a14cb5a125009",
+    "url": "/static/js/runtime-main.ca8221b6.js"
+  },
+  {
+    "revision": "17b79c5e3c523cfea588c7cd14fee9eb",
+    "url": "/static/media/NYPizza.17b79c5e.jpg"
+  },
+  {
+    "revision": "a089421dd3338142298818ba729e0885",
+    "url": "/static/media/OWchi.a089421d.png"
+  },
+  {
+    "revision": "4625e5b1111eb4fdb6e89db0f00ca9cb",
+    "url": "/static/media/coin.4625e5b1.svg"
+  },
+  {
+    "revision": "26de3901fd2a159ef6a8d388dddcf3a0",
+    "url": "/static/media/king.26de3901.svg"
+  },
+  {
+    "revision": "b1f5936eaa319551ce015f781d49c9d7",
+    "url": "/static/media/me.b1f5936e.png"
+  },
+  {
+    "revision": "ff375205c6cbcb5b9c1e1c3adc915da2",
+    "url": "/static/media/outoften.ff375205.png"
+  },
+  {
+    "revision": "5d6153804711429db9b5911c5775e285",
+    "url": "/static/media/rocket.5d615380.svg"
+  },
+  {
+    "revision": "952d0714c741cf5a80d5b0f4077f6a2b",
+    "url": "/static/media/yellow.952d0714.png"
+  }
+]);
